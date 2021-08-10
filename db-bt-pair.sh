33840b83cc74d911f5483d49cf9fc5bafd91c643
Siddharth R. Shete <86878145+siddharth-03s@users.noreply.github.com>
2021-08-10 22:55:33 +0530

    Add files via upload

---
+++ db-bt-pair.sh
@@ -0,0 +1,10 @@
+cp -i /var/lib/bluetooth/70:66:55:B3:A6:D4/11:22:16:54:7C:EB/info /home/siddharth/Drives/zArcolinux-mnt/var/lib/bluetooth/70:66:55:B3:A6:D4/11:22:16:54:7C:EB/
+echo "Finished copyinf Bluetooth Pairing File to Arcolinux ..."
+
+cp -i	 /var/lib/bluetooth/70:66:55:B3:A6:D4/11:22:16:54:7C:EB/info /home/siddharth/Drives/zEndeavour-mnt/var/lib/bluetooth/70:66:55:B3:A6:D4/11:22:16:54:7C:EB/
+echo "Finished copying Bluetooth Pairing File to EndeavourOS ..."
+
+
+
+
+
